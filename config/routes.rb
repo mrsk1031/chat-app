8cb860d05e94f424bc566e2d66cc0cc04c394e86
mrsk1031 <ryosuke031031@icloud.com>
2021-02-28 03:59:34 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "messages#index"
   resources :users, only: [:edit, :update]
+  resources :rooms, only: [:new, :create]
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
