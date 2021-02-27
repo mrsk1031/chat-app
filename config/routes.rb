3d960330f71ac26dda076f434af63bbef49732c6
mrsk1031 <ryosuke031031@icloud.com>
2021-02-28 07:52:16 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "rooms#index"
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create]
+  resources :rooms, only: [:new, :create] do
+    resources :messages, only: [:index, :create]
+  end
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
